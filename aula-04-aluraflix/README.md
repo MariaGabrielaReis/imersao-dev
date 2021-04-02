e9898533f5c1320de5732e51cbeffaf576768b07
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-02 11:03:27 -0300

    Adição do projeto realizado na aula 04

---
+++ aula-04-aluraflix/README.md
@@ -0,0 +1,6 @@
+# AluraFlix - Catálogo de capas de filmes (estático)
+
+Código criado na aula 4 do evento "Imersão Dev", produzido pela Alura. 
+- Foi utilizado a plataforma CodePen.io, podendo ser conferido [neste link](https://codepen.io/mariagabrielareis/pen/JjEWwMm).
+
+
---
+++ aula-04-aluraflix/src/script.js
@@ -0,0 +1,5 @@
+var filmes = ["https://m.media-amazon.com/images/M/MV5BOTgwMzFiMWYtZDhlNS00ODNkLWJiODAtZDVhNzgyNzJhYjQ4L2ltYWdlXkEyXkFqcGdeQXVyNzEzOTYxNTQ@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTY0MjEyODQzMF5BMl5BanBnXkFtZTcwMTczMjQ4Mg@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+for (var i = 0; i < filmes.length; i++) {
+  document.write("<img src=" + filmes[i] + ">")
+}
