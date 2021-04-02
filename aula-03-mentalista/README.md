4739c52a8b3388fccba31ff74d29d9b816339908
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-01 21:50:55 -0300

    Adição do projeto realizado na aula 03

---
+++ aula-03-mentalista/README.md
@@ -0,0 +1,6 @@
+# Mentalista - Adivinhe um nº entre 0 e 10
+
+Código criado na aula 3 do evento "Imersão Dev", produzido pela Alura. 
+- Foi utilizado a plataforma CodePen.io, podendo ser conferido [neste link](https://codepen.io/mariagabrielareis/pen/XWpMeWo).
+
+
---
+++ aula-03-mentalista/src/script.js
@@ -0,0 +1,21 @@
+﻿var numSecreto = parseInt(Math.random() * 10) // multiplica por 10 pq o random é só de nºs entre 0 e 1
+var tentativas = 3
+
+while (tentativas > 0) {
+  var chute = parseInt(prompt("Digite um nº entre 0 e 10: "))
+  
+  if(chute == numSecreto) {
+    alert("Acertou!!")
+    break
+  } else if (chute > numSecreto){
+    alert("Chute muito alto!")
+    tentativas = tentativas - 1
+  } else if (chute < numSecreto){
+    alert("Chute muito baixo!")
+    tentativas = tentativas - 1
+  } 
+}
+
+if (chute != numSecreto) {
+  console.log("Suas tentativas acabaram, o nº secreto era " + numSecreto)
+}
